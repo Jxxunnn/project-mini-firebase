dd334ead6bf0424076ba9ecb30a0d0fe13371f01
Jxxunnn <roof2586@naver.com>
2022-12-06 15:30:26 +0900

    feat: 회원가입 기능 구현

---
+++ my-diary/src/context/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+
+const AuthContext = createContext();
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextProvider };
